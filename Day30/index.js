e34e0a763e3737c341b4ce9d5db42a13ee9fc8b6
Meherun Akter <116157144+meherun05@users.noreply.github.com>
2022-12-19 17:21:43 +0600

    Add files via upload

---
+++ Day30/index.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    document.getElementById("demo4").innerHTML = "Paragraph changed.";
+  }
+
+  function f() {
+    document.getElementById("demo5").innerHTML = "Hello Dear";
+    document.getElementById("demo6").innerHTML = "How Are You";
+  }
+
+let x,y;
+x=200;
+y=110;
+z=x+y;
+
+document.getElementById('demo7').innerHTML='The sumof x and y ='+' '+ z;
+
+const cars = ["Saab", "Volvo", "BMW"];
+document.getElementById('demo8').innerHTML= cars[1] +' '+cars[2];
+// document.getElementById('demo9').innerHTML= cars[2];
+
